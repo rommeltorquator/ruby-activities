6b77627820666cc9460db705663022cbe6cb3432
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-07 06:38:32 -0400

    Modified Polymorphism with Inheritance and Duck Typing activity

--- activity-10/app.rb
+++ activity-10/app.rb
@@ -1,30 +1,49 @@
-# Polymorphism with Duck Typing and Inheritance
+# Polymorphism with Inheritance
 
-class Pizza
-    def bake
-        "Now baking"
+class Car
+    def initialize(make, model)
+        @make = make
+        @model = model
     end
 
+    def beep
+        puts "Beep Beep Beep!"
+    end
+end
+
+class Innova < Car
+    def beep
+        puts "Beeeeeeeeep Beeeeeeeeep!"
+    end
+end
+
+class City < Car
+    def beep
+        puts "Beep!"
+    end
+end
+
+innova = Innova.new("Toyota", "Innova")
+innova.beep
+
+city = City.new("Honda", "City")
+city.beep
+
+# Polymorphism with Duck Typing
+
+class Pizza
     def crust_size(size)
         size.crust_size
     end
 end
 
 class ThinCrust < Pizza
-    def bake
-        "#{crust_size} is now baking"
-    end
-
     def crust_size
         "Thin Crust Pizza"
     end
 end
 
 class ThickCrust < Pizza
-    def bake
-        "#{crust_size} is now baking"
-    end
-
     def crust_size
         "Thick Crust Pizza"
     end
@@ -35,9 +54,6 @@ managers_choice = Pizza.new
 
 thin_crust = ThinCrust.new
 thick_crust = ThickCrust.new
-
-puts thin_crust.bake
-puts thick_crust.bake
 
 puts belly_buster.crust_size(thin_crust)
 puts managers_choice.crust_size(thick_crust)
