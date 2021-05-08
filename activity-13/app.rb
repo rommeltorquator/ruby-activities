2ee73f0f13d6c80f29e5967432394ee7429c2e4e
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-08 00:49:53 -0400

    Modified the Coding Activity for May 7, 2021

--- activity-13/app.rb
+++ activity-13/app.rb
@@ -1,12 +1,19 @@
-positive = 1
-negative = 0
-arr = []
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15].each do |num|
-    positive += 1 if num >= 0
-    negative += num if num <= 0
+def count_positives_sum_negatives(lst)
+    #your code here
+    positive = 0
+    negative = 0
+    arr = []
+    
+    lst.each do |num|
+        positive += 1 if num > 0
+        negative += num if num <= 0
+    end
+    if lst.empty? == true
+      arr = []
+    else
+      arr.push(positive)
+      arr.push(negative)
+    end
 end
-arr.push(positive)
-arr.push(negative)
-
-p arr
