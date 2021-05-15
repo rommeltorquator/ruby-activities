9d5bdfdd25caf4d8c0e6b995f467cc4528c0f2e8
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-15 01:19:22 -0400

    Modified the Ruby Coding Activity for May 12, 2021

--- activity-18/app.rb
+++ activity-18/app.rb
@@ -1,19 +1,9 @@
 def century(year)
     # Your solution goes here, warrior
-    case year
-    when (2001..2100)
-        21
-    when (1901..2000)
-        20
-    when (1801..1900)
-        19
-    when (1701..1800)
-        18
-    when (1601..1700)
-        17
-    when (1501..1600)
-        16
-    when (1..1599)
-        1
-    end
+    ans = 0.0
+    ans = year.to_f / 100
+    dec = ans.to_s.split('.')
+
+    ans = ans.to_i + 1 if dec[1] != '0'
+    ans
 end
